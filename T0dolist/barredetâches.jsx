897c45fd37c6826d316e13e5ea459158e88c9a01
raam's raam's <ramy.cherrouk@gmail.com>
2025-02-16 17:25:02 +0100

    ajout placeholder

--- T0dolist/barredetâches.jsx
+++ T0dolist/barredetâches.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 function barre() {
     const [tâches, setTâches]= useState([]);
-    const [newTâches, setNewTâches]=useState('')
+    const [newTâches, setNewTâches]=useState('');
+
+    function ajoutTâche(){
+        if (newTâches){
+            setTâches
+        }
+    }
 }
 
 return (
     <div>
         <h1>T0dolist</h1>
-        <input type= "text"/>
+        <input type= "text"
+        Value={newTâches}
+        placeholder="Ajoutez une tâche"/>
     </div>
 )
+
+export default barre;
