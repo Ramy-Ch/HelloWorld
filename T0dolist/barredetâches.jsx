0da265dbf175f6da65d1f3d8943f8d02ece9b69c
raam's raam's <ramy.cherrouk@gmail.com>
2025-02-16 17:10:51 +0100

    Essai d'ajout de zone de texte

---
+++ T0dolist/barredetâches.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+function barre() {
+    const [tâches, setTâches]= useState([]);
+    const [newTâches, setNewTâches]=useState('')
+}
+
+return (
+    <div>
+        <h1>T0dolist</h1>
+        <input type= "text"/>
+    </div>
+)
