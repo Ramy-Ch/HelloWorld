0ee4b16ba871428cf272571e202b700512e6905c
raam's raam's <ramy.cherrouk@gmail.com>
2025-02-16 20:07:47 +0100

    oublie bouton suppression

--- T0dolist/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Barredetâches from "./components/barredetâches";
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-      <Barredetâches/>,
-    document.getElementById('root') 
-  )
--- T0dolist/src/App.jsx
+++ T0dolist/src/App.jsx
@@ -1,35 +1,34 @@
-import react, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css';
 
-function barre() {
-  const [tâches, setTâches]= useState([]);
-  const [newTâches, setNewTâches]=useState('');
+function Barre() {
+  const [tâches, setTâches] = useState([]);
+  const [newTâches, setNewTâches] = useState('');
 
-  function ajoutTâche(){
-      if (newTâches){
-          setTâches([...tâches, newTâches]);
-          setNewTâches('')
-      }
+  function ajoutTâche() {
+    if (newTâches) {
+      setTâches([...tâches, newTâches]);
+      setNewTâches('');
+    }
   }
+
+  return (
+    <div>
+      <h1>T0dolist</h1>
+      <input
+        type="text"
+        value={newTâches}  
+        onChange={(e) => setNewTâches(e.target.value)}
+        placeholder="Ajoutez une tâche"
+      />
+      <button onClick={ajoutTâche}>Ajouter</button>
+      <ul>
+        {tâches.map((tache, index) => (
+          <li key={index}>{tache}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-return (
-  <div>
-      <h1>T0dolist</h1>
-      <input type= "text"
-      Value={newTâches}
-      onChange={(e)=>setNewTâches(e.target.value)}
-      placeholder="Ajoutez une tâche"
-      />
-      <button onClick={ajoutTâche}>Add</button>
-      <ul>
-      {taches.map((tache, index) => (
-        <li key={index}>{tache}</li>
-      ))}
-    </ul>
-  </div>
-)
-
-export default barre;
+export default Barre;
