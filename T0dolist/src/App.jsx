2b9d566b04bb8d9576478d000608704f8602e099
Ramy <ramycherrouk@gmail.com>
2025-03-20 13:54:17 +0100

    récupération bannière

--- T0dolist/src/App.jsx
+++ T0dolist/src/App.jsx
@@ -5,19 +5,19 @@ import Navbar from "./Navbar";
 import Calculator from "./Calculator";
 import "./index.css";
 import Chronometer from "./Chronometer";
-
-
+import Banner from "./Banner"; // Importation du composant Banner
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Banner /> 
       <div className="flex items-center justify-center h-screen">
-      <Routes>
+        <Routes>
           <Route path="/" element={<div className="main-content"><h1>Bienvenue</h1></div>} />
           <Route path="/ToDoList" element={<ToDoList />} />
           <Route path="/calculator" element={<Calculator />} />
-          <Route path="/chronometer" element={<Chronometer />} /> {/* Route ajoutée */}
+          <Route path="/chronometer" element={<Chronometer />} />
         </Routes>
       </div>
     </div>
---
+++ T0dolist/src/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-text">Toutes mes condoléances pour le PC</div>
+    </div>
+  );
+};
+
+export default Banner;
